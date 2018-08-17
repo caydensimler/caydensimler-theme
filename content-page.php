070db900b4478a5c2278ac53acf1a5db808ff02a
Cayden <cayden.simler@hotmail.com>
2018-08-17 13:10:40 -0500

    Fixed bootstrap location problems with child theme.

--- content-page.php
+++ content-page.php
@@ -48,52 +48,8 @@ if ( ! defined( 'ABSPATH' ) ) {
 				'before' => '<div class="page-links">' . __( 'Pages:', 'generatepress' ),
 				'after'  => '</div>',
 			) );
+			wp_reset_query();
 			?>
-
-			<div class="section_content">
-
-				<!-- About Me/Interests -->
-				<h1 class="section_header" id="about">
-					<?php the_field( 'interest_header' ); ?>
-					<hr>
-				</h1>
-
-				<?php if ( have_rows( 'interest_single' ) ) : ?>
-					<?php while ( have_rows( 'interest_single' ) ) : the_row(); ?>
-						<div class="col-xs-12 col-md-6 col-lg-4 interest_single">
-							<!-- <div class="interest_single_header" style="background-image: url(<?php the_sub_field( 'interest_single_background_image' ); ?>"> -->
-							<div class="interest_single_header" style="background:linear-gradient(0deg,rgba(0,0,0,0.4),rgba(0,0,0,0.4)),url(<?php the_sub_field( 'interest_single_background_image' ); ?>);">
-								<h1>
-									<span class="interest_icon"><?php the_sub_field( 'interest_single_icon' ); ?></span>
-								</h1>
-								<h2><?php the_sub_field( 'interest_single_header' ); ?></h2>
-							</div>
-
-							<p class="interest_text"><?php the_sub_field( 'interest_single_text' ); ?></p>
-						</div>
-					<?php endwhile; ?>
-				<?php else : ?>
-					<?php // no rows found ?>
-				<?php endif; ?>
-
-				<!-- Portfolio -->
-				<?php
-					// Get the 'Profiles' post type
-					// $args = array(
-					//     'post_type' => 'projects',
-					// );
-					// $loop = new WP_Query($args);
-
-					// while($loop->have_posts()): $loop->the_post();
-
-					// the_title();
-					// the_category();
-					// echo '<img src="' . get_the_post_thumbnail_url() . '">';
-					// echo '<br>';
-
-					// endwhile;
-					// wp_reset_query();
-				?>
 
 		</div><!-- .entry-content -->
 
--- header.php
+++ header.php
@@ -15,10 +15,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
 	<link rel="profile" href="http://gmpg.org/xfn/11">
-	<!-- Latest compiled and minified CSS -->
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-	<?php wp_head(); ?>
-
 
 </head>
 
